bb49170fee9df43eec7592857c59d75da9ea11d1
TRAVALIN <xyz3d@list.ru>
2024-02-03 22:01:12 +0300

    add ITPL_Seminar2:Task1-Task4

---
+++ ITPL_Seminar2/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Программа которая удаляет вторую цифру
+трёхзначного числа 
+а = 256 => 26
+a = 891 => 81
+*/
+
+int a = 256;
+int lastDigit = a % 10;
+int FirstDigit = a / 100;
+Console.Write(FirstDigit+ "" +lastDigit);
---
+++ ITPL_Seminar2/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное
+число и возводит вторую цифру этого числа в степень, равную
+третьей цифре.
+Примеры
+487 => 8^7 = 2 097 152
+254 => 5^4 = 625
+617 => 1 */
+
+
+/* в папке Math хранятся различные математические функции
+int result = Math.Sqrt(    ); // квадратный корень
+double result = Math.Pow(8, 7); // возведение в степень дабл потому что могут быть не целые числа
+*/
+
+int a = 487;
+int secondDigit = a / 10 % 10;
+int thirdDigit = a % 10;
+double result = Math.Pow(secondDigit, thirdDigit);
+Console.WriteLine(result);
+Console.WriteLine(thirdDigit);
+Console.WriteLine(secondDigit);
+
+int pow = 1;
+int count = 0;
+
+while (count < thirdDigit)
+{
+    pow = pow * secondDigit;
+    count++;
+}
+Console.WriteLine(pow);
+
+int pow2 = 1;
+for (int i = 0; i < thirdDigit; i++)
+{
+    //pow2 = pow2 * secondDigit;
+    pow2 *= secondDigit;
+}
+Console.WriteLine(pow2);
