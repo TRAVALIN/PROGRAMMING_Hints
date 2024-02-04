b7bd549e82ce5a95a46eb74d30137919d0ad3ede
TRAVALIN <xyz3d@list.ru>
2024-02-04 15:17:48 +0300

    add itpl_seminar2:task1-task5

---
+++ ITPL_Seminar2/HW_Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 3: Напишите программу, 
+которая принимает на вход целое число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+40 => 4
+96 => 9
+72 => 7
+*/
+
+int a = 79;
+int firstDigit = a / 10;
+int secondDigit = a % 10;
+
+if (a >= 10 & a <=99)
+{
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+else
+{
+    Console.WriteLine("недопустимое значение");
+}
+
+
---
+++ ITPL_Seminar2/HW_Task5/Program.cs
@@ -0,0 +1,41 @@
+﻿/* ПОДСКАЗКИ ДЛЯ ВЫПОЛНЕНИЯ ДОМАШНЕЙ РАБОТЫ
+К СЕМИНАРУ 2 
+
+/* в условии знак & = и    знак | = или
+знак ! = не
+
+int a = 123; 
+if (!(a < 100 & a < 1000) & a % 23 == 0) - при такой записи все условия должны быть true (истина)
+if (a < 100 && a < 1000) - при такой записи если хотябы один результат true (истина),
+то и весь результат будет true (истина)
+тоже самое и для знаков !! || 
+*/
+
+
+int b = 12345;
+Console.WriteLine(b.ToString().Length);
+
+int a = 123456;
+int countDigit = 0;
+int x = a;
+
+while (x != 0)
+{
+   x /= 10; // результат деления х на 10 или x = x / 10
+   countDigit++;
+}
+Console.WriteLine(countDigit);
+
+
+int a1 = 123456;
+//Console.WriteLine(a.ToString().length);
+int countDigit1 = 0;
+int x1 = a1;
+
+while (x1 != 0)
+{
+   Console.WriteLine(x1);
+   x1 = x1 / 10; // можно записать: x1 /= 10; - результат деления х1 на 10
+   countDigit1++;
+}
+Console.WriteLine(countDigit1);
