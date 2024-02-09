5ea143dcf2e846b109bac853cf4dbf94a1457715
TRAVALIN <xyz3d@list.ru>
2024-02-10 00:51:14 +0300

    add Code lesson3:task4

--- ITPL_Lectures/lesson3/Task4/Program.cs
+++ ITPL_Lectures/lesson3/Task4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задание данных в массив с консоли */
+
+int size = 10;
+int[] arr = new int[size];
+int i = 0;
+while (i < size)
+{
+    Console.WriteLine("Введите целое число:");
+    string input = Console.ReadLine();
+    arr[i] = Convert.ToInt32(input);
+    i++;
+}
+foreach (var item in arr)
+{
+    Console.Write(item + " ");
+}
