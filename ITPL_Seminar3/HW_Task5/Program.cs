e1d8be674c43f59c7899c059ca983e7e82bab6db
TRAVALIN <xyz3d@list.ru>
2024-02-09 19:21:32 +0300

    add code in ITPL_Seminar3:HW_Task4

--- ITPL_Seminar3/HW_Task5/Program.cs
+++ ITPL_Seminar3/HW_Task5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 123456;
+int x = a;
+int countDigit = 0;
+while (x != 0)
+{
+    x = x / 10;
+    countDigit++;
+}
+int[] arr = new int[countDigit];
+int i = 0;
+int x1 = a;
+while (i < countDigit)
+{
+    arr[i] = x1 % 10;
+    Console.Write(arr[i] + " ");
+    x1 = x1 / 10;
+    //Console.WriteLine(x1);
+    i = i + 1;
+}
+
